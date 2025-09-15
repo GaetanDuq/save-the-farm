652fd7d2989ccd4b7949aad274bce9ed6e7b57e5
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-09-15 21:47:48 +0900

    program works. Can refractor more though

--- cookSoup.php
+++ cookSoup.php
@@ -4,7 +4,7 @@ function cookSoup(){
 	// Write your code here:
   global $location, $has_mushrooms, $has_soup;
 
-  if (!($location === "kitchen" && $has_mushrooms === TRUE)) {
+  if ($location !== "kitchen" && !$has_mushrooms) {
     print "You can't cook like this! You need something to cook AND to be in the kitchen.\n";
   } else {
     print "You made some mushroom soup. Mushroom is the queen of all soups!\n";

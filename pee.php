7cb85c3bcfe79ba6e1d8d617c616a3cf202e263f
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-09-15 21:01:06 +0900

    pee function done

--- pee.php
+++ pee.php
@@ -2,10 +2,13 @@
 
 function pee(){
 	// Write your code here:
+  global $location, $needs_to_pee;
 
-
-
-
-
+  if ($location === "bathroom" || $location === "woods") {
+    print "You relieve yourself.\n";
+    $needs_to_pee = FALSE;
+  } else {
+    print "Are you crazy? You can't pee here!\n";
+  };
 
 }

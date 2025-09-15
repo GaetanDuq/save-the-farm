ac5b5c05eea0634b40e5d28a417d5974b407655f
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-09-15 20:58:56 +0900

    eatSoup function done

--- eatSoup.php
+++ eatSoup.php
@@ -1,10 +1,14 @@
 <?php
 function eatSoup(){
 	// Write your code here:
+  global $has_soup, $is_hungry;
 
-
-
-
-
+  if ($has_soup === FALSE) {
+    print "You don't have any cooked food to eat!\n";
+  } else {
+    print "You have eaten the soup!\n";
+    $has_soup = FALSE;
+    $is_hungry = FALSE;
+  };
 
 }
